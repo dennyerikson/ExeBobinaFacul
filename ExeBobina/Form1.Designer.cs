34897eb7f9d17e4f975c2e8bb74036bdd03c7202
dennyerikson <dennyeriks00on@gmail.com>
2018-08-09 00:51:58 -0300

    first Commit exeBobina

---
+++ ExeBobina/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ExeBobina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPesoTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbValorMadidas = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbPesoTotal
+            // 
+            this.tbPesoTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPesoTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tbPesoTotal.Location = new System.Drawing.Point(153, 28);
+            this.tbPesoTotal.Name = "tbPesoTotal";
+            this.tbPesoTotal.Size = new System.Drawing.Size(387, 32);
+            this.tbPesoTotal.TabIndex = 0;
+            this.tbPesoTotal.Text = "4500";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Peso total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(38, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valor das medidas";
+            // 
+            // tbValorMadidas
+            // 
+            this.tbValorMadidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbValorMadidas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tbValorMadidas.Location = new System.Drawing.Point(43, 94);
+            this.tbValorMadidas.Multiline = true;
+            this.tbValorMadidas.Name = "tbValorMadidas";
+            this.tbValorMadidas.Size = new System.Drawing.Size(497, 81);
+            this.tbValorMadidas.TabIndex = 2;
+            this.tbValorMadidas.Text = "357,357,264,259,163";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(75, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "*Obs. Separadas por virgula, ok?";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DimGray;
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.FlatAppearance.BorderSize = 0;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(219, 210);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(134, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "=";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResp
+            // 
+            this.lblResp.AutoSize = true;
+            this.lblResp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblResp.Location = new System.Drawing.Point(38, 241);
+            this.lblResp.Name = "lblResp";
+            this.lblResp.Size = new System.Drawing.Size(193, 26);
+            this.lblResp.TabIndex = 6;
+            this.lblResp.Text = "Valor das medidas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 489);
+            this.Controls.Add(this.lblResp);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbValorMadidas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPesoTotal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPesoTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbValorMadidas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResp;
+    }
+}
+
---
+++ ExeBobina/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExeBobina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        List<string> listMedidas = new List<string>();
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            GetMedidas(tbValorMadidas.Text);
+
+            double resp = Convert.ToDouble(tbPesoTotal.Text) / aux;
+            double pesoTotal = 0;
+            for(int i =0; i < listMedidas.Count; i++)
+            {
+                lblResp.Text += "\n Medida: "+listMedidas[i]+" = "+
+                    (Convert.ToDouble(listMedidas[i]) * resp);
+                pesoTotal += Convert.ToDouble(listMedidas[i]);
+            }
+
+            lblResp.Text += "\n Peso total: " + pesoTotal * listMedidas.Count + " \n Resp: " + resp ;
+
+        }
+
+        double aux;
+        public List<string> GetMedidas(string medidas)
+        {
+            string[] list = medidas.Split(',');
+
+            foreach(string lists in list)
+            {
+                listMedidas.Add(lists);
+              
+            }
+
+            for(int i = 0; i < listMedidas.Count; i++)
+            {
+                aux = Convert.ToDouble(listMedidas[i]);
+            }
+            
+
+            return listMedidas;
+        }
+    }
+}
