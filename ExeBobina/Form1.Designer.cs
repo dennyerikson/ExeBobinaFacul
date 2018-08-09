571ecdf4a41a7234e633b1e01ce63c2d5037bcbb
dennyerikson <dennyeriks00on@gmail.com>
2018-08-09 15:39:25 -0300

    Ajustes formula

--- ExeBobina/Form1.Designer.cs
+++ ExeBobina/Form1.Designer.cs
@@ -35,61 +35,64 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.lblResp = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbPesoTotal
             // 
-            this.tbPesoTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPesoTotal.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbPesoTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.tbPesoTotal.Location = new System.Drawing.Point(153, 28);
+            this.tbPesoTotal.Location = new System.Drawing.Point(158, 71);
             this.tbPesoTotal.Name = "tbPesoTotal";
-            this.tbPesoTotal.Size = new System.Drawing.Size(387, 32);
+            this.tbPesoTotal.Size = new System.Drawing.Size(440, 33);
             this.tbPesoTotal.TabIndex = 0;
             this.tbPesoTotal.Text = "4500";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Location = new System.Drawing.Point(43, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 26);
+            this.label1.Size = new System.Drawing.Size(111, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Peso total";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(38, 65);
+            this.label2.Location = new System.Drawing.Point(43, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(193, 26);
+            this.label2.Size = new System.Drawing.Size(200, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Valor das medidas";
             // 
             // tbValorMadidas
             // 
-            this.tbValorMadidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbValorMadidas.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbValorMadidas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.tbValorMadidas.Location = new System.Drawing.Point(43, 94);
+            this.tbValorMadidas.Location = new System.Drawing.Point(48, 137);
             this.tbValorMadidas.Multiline = true;
             this.tbValorMadidas.Name = "tbValorMadidas";
-            this.tbValorMadidas.Size = new System.Drawing.Size(497, 81);
+            this.tbValorMadidas.Size = new System.Drawing.Size(550, 81);
             this.tbValorMadidas.TabIndex = 2;
             this.tbValorMadidas.Text = "357,357,264,259,163";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label3.Location = new System.Drawing.Point(75, 178);
+            this.label3.Location = new System.Drawing.Point(80, 221);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(226, 18);
+            this.label3.Size = new System.Drawing.Size(404, 24);
             this.label3.TabIndex = 4;
-            this.label3.Text = "*Obs. Separadas por virgula, ok?";
+            this.label3.Text = "*Obs. Separar medidas por virgula, ok?";
             // 
             // btnCalcular
             // 
@@ -97,10 +100,11 @@
             this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCalcular.FlatAppearance.BorderSize = 0;
             this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCalcular.ForeColor = System.Drawing.Color.White;
-            this.btnCalcular.Location = new System.Drawing.Point(219, 210);
+            this.btnCalcular.Location = new System.Drawing.Point(224, 253);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(134, 23);
+            this.btnCalcular.Size = new System.Drawing.Size(134, 35);
             this.btnCalcular.TabIndex = 5;
             this.btnCalcular.Text = "=";
             this.btnCalcular.UseVisualStyleBackColor = false;
@@ -109,19 +113,41 @@
             // lblResp
             // 
             this.lblResp.AutoSize = true;
-            this.lblResp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResp.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblResp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblResp.Location = new System.Drawing.Point(38, 241);
+            this.lblResp.Location = new System.Drawing.Point(43, 292);
             this.lblResp.Name = "lblResp";
-            this.lblResp.Size = new System.Drawing.Size(193, 26);
+            this.lblResp.Size = new System.Drawing.Size(200, 24);
             this.lblResp.TabIndex = 6;
             this.lblResp.Text = "Valor das medidas";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(645, 60);
+            this.panel1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(235, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Valor das medidas";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 489);
+            this.ClientSize = new System.Drawing.Size(645, 606);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblResp);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.label3);
@@ -130,7 +156,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbPesoTotal);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,6 +175,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Label lblResp;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- ExeBobina/Form1.cs
+++ ExeBobina/Form1.cs
@@ -23,39 +23,64 @@ namespace ExeBobina
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            GetMedidas(tbValorMadidas.Text);
-
-            double resp = Convert.ToDouble(tbPesoTotal.Text) / aux;
-            double pesoTotal = 0;
-            for(int i =0; i < listMedidas.Count; i++)
+            switch (btnCalcular.Text)
             {
-                lblResp.Text += "\n Medida: "+listMedidas[i]+" = "+
-                    (Convert.ToDouble(listMedidas[i]) * resp);
-                pesoTotal += Convert.ToDouble(listMedidas[i]);
+                case "=":
+                    // obtendo o valor das medidas 08/08
+                    GetMedidas(tbValorMadidas.Text);
+
+                    // peso total dividido pela soma das medidas 08/08
+                    double resp = Convert.ToDouble(tbPesoTotal.Text) / aux;
+                    double pesoTotal = 0;
+                    for (int i = 0; i < listMedidas.Count; i++)
+                    {
+                        lblResp.Text += "\n Medida: " + listMedidas[i] + " = Peso: " +
+                            // multiplicação das medias por retorno de "resp" 09/08/2018
+                             (resp * (Convert.ToDouble(listMedidas[i]))).ToString("n2") +" kg";
+                        // soma dos pesos 09/08/2018
+                        (pesoTotal += (resp * (Convert.ToDouble(listMedidas[i])))).ToString("n2");
+                    }
+                    lblResp.Text += "\n\nPeso total: " + pesoTotal +" kg"+ " \nPeso por mm: " + resp.ToString("n4")+
+                        "\n\nFormula: (valor_medida * (peso_total / soma_medidas))"+
+                        "\nCréditos a formula Alcides Galdino";
+
+                    btnCalcular.Text = "↑↓";
+                    break;
+
+                case "↑↓":
+
+                    tbPesoTotal.Text = "";
+                    tbValorMadidas.Text = "";
+                    lblResp.Text = "";
+                    btnCalcular.Text = "=";
+
+                    break;
             }
-
-            lblResp.Text += "\n Peso total: " + pesoTotal * listMedidas.Count + " \n Resp: " + resp ;
-
         }
 
         double aux;
+        // carregar lista
         public List<string> GetMedidas(string medidas)
         {
+            // remoção das virgulas para a lista
             string[] list = medidas.Split(',');
 
             foreach(string lists in list)
             {
-                listMedidas.Add(lists);
-              
+                listMedidas.Add(lists);              
             }
 
             for(int i = 0; i < listMedidas.Count; i++)
             {
-                aux = Convert.ToDouble(listMedidas[i]);
+                // soma das medias da lista 
+                aux += Convert.ToDouble(listMedidas[i]);
             }
-            
-
             return listMedidas;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
